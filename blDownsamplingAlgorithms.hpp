1aab5c801c492e18e756df895676440b83de5536
Vincenzo Barbato <navyenzo@gmail.com>
2017-03-14 11:18:30 -0400

    Downsampling new function
    -- introduced new largestTriangleThreeBuckets function to downsample y-axis data and get corrisponding x-axis data points with it

--- blDownsamplingAlgorithms.hpp
+++ blDownsamplingAlgorithms.hpp
@@ -246,4 +246,177 @@ inline void largestTriangleThreeBuckets(srcDataIteratorType srcDataBegin,
 //-------------------------------------------------------------------
 
 
+//-------------------------------------------------------------------
+// The following function downsamples a source
+// into the destination container by use of the
+// Largest-Triangle-Three-Buckets algorithm like above
+// The difference is that this function takes an x-axis
+// as well as y-axis and samples the y-axis while also
+// taking the x-axis points corresponding to the sampled
+// y-axis points
+//-------------------------------------------------------------------
+template<typename xSrcDataIteratorType,
+         typename ySrcDataIteratorType,
+         typename xDstDataIteratorType,
+         typename yDstDataIteratorType>
+
+inline void largestTriangleThreeBuckets(xSrcDataIteratorType xSrcDataBegin,
+                                        ySrcDataIteratorType ySrcDataBegin,
+                                        const size_t& srcDataSize,
+                                        xDstDataIteratorType xDstDataBegin,
+                                        yDstDataIteratorType yDstDataBegin,
+                                        const size_t& dstDataSize)
+{
+    if(srcDataSize <= 0 || dstDataSize <= 0)
+    {
+        // We have no data to downsample
+
+        return;
+    }
+    else if(dstDataSize >= srcDataSize)
+    {
+        // In this case we simply copy
+        // the source data into the
+        // destination data
+
+        for(size_t i = 0,j = 0; i < srcDataSize && j < dstDataSize; ++i,++j)
+        {
+            (*xDstDataBegin) = (*xSrcDataBegin);
+            (*yDstDataBegin) = (*ySrcDataBegin);
+
+            ++xSrcDataBegin;
+            ++ySrcDataBegin;
+
+            ++xDstDataBegin;
+            ++yDstDataBegin;
+        }
+
+        return;
+    }
+    else if(dstDataSize == 1)
+    {
+        // In this case we just
+        // copy the first data point
+
+        (*xDstDataBegin) = (*xSrcDataBegin);
+        (*yDstDataBegin) = (*ySrcDataBegin);
+
+        return;
+    }
+
+
+
+    // Now we got all the special
+    // cases out of the way
+
+
+
+    // Calculate the size of each bucket
+
+    size_t bucketStep = srcDataSize / dstDataSize;
+
+    // Create temporary point values
+    // used to find the point to sample
+
+    auto yPreviousBucketPoint = (*ySrcDataBegin);
+    auto yNextBucketAvgValue = yPreviousBucketPoint;
+
+    // Temporary variables used to find
+    // the max triangle area formed by
+    // the sampled points
+
+    auto maxTriangleArea = yPreviousBucketPoint;
+    auto currentTriangleArea = maxTriangleArea;
+
+    // Iterators used to move through the
+    // source data n buckets at a time
+
+    auto xSrcCurrentBucketIterator = xSrcDataBegin;
+    auto ySrcCurrentBucketIterator = ySrcDataBegin;
+
+    auto ySrcNextBucketIterator = ySrcDataBegin;
+    std::advance(ySrcNextBucketIterator,1 + bucketStep);
+
+    // Value of 2 used to calculate
+    // the triangle area
+
+    auto two = static_cast<decltype(yPreviousBucketPoint)>(2);
+
+
+
+    // Let's step through the data and
+    // sample the points of interest
+
+    for(size_t bucketIndex = 0; bucketIndex < dstDataSize; ++bucketIndex)
+    {
+        // If we are in the first or
+        // last buckets then we just
+        // copy the first or last data
+        // point
+
+        if(bucketIndex == 0)
+        {
+            (*xDstDataBegin) = (*xSrcDataBegin);
+            (*yDstDataBegin) = (*ySrcDataBegin);
+
+            ++xSrcCurrentBucketIterator;
+            ++ySrcCurrentBucketIterator;
+        }
+        else if(bucketIndex == dstDataSize - 1)
+        {
+            std::advance(xSrcDataBegin,srcDataSize - 1);
+            std::advance(ySrcDataBegin,srcDataSize - 1);
+
+            (*xDstDataBegin) = (*xSrcDataBegin);
+            (*yDstDataBegin) = (*ySrcDataBegin);
+        }
+        else
+        {
+            // Calculate the average value
+            // of the next bucket
+
+            yNextBucketAvgValue = 0;
+
+            for(size_t i = 0; i < bucketStep; ++i)
+            {
+                yNextBucketAvgValue += (*ySrcNextBucketIterator);
+                ++ySrcNextBucketIterator;
+            }
+
+            yNextBucketAvgValue /= static_cast<decltype(yNextBucketAvgValue)>(bucketStep);
+
+            // Calculate which point gives
+            // the triangle with the biggest
+            // area and choose that point as
+            // the point for this current bucket
+
+            maxTriangleArea = 0;
+            currentTriangleArea = 0;
+
+            for(size_t i = 0; i < bucketStep; ++i)
+            {
+                currentTriangleArea = std::abs( (yNextBucketAvgValue - yPreviousBucketPoint) + two*(yPreviousBucketPoint - (*ySrcCurrentBucketIterator)) ) / two;
+
+                if(currentTriangleArea > maxTriangleArea)
+                {
+                    maxTriangleArea = currentTriangleArea;
+
+                    (*xDstDataBegin) = (*xSrcCurrentBucketIterator);
+                    (*yDstDataBegin) = (*ySrcCurrentBucketIterator);
+                }
+
+                ++xSrcCurrentBucketIterator;
+                ++ySrcCurrentBucketIterator;
+            }
+        }
+
+        // Advance to the next destination point
+
+        ++xDstDataBegin;
+        ++yDstDataBegin;
+    }
+}
+//-------------------------------------------------------------------
+
+
 #endif // BL_DOWNSAMPLINGALGORITHMS_HPP
